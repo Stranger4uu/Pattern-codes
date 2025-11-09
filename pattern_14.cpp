c63feb9995bc9ab09a20a2d7a52a392cea803d9f
Stranger4uu <yashsaini4824@gmail.com>
2025-11-09 22:39:46 +0530

    added new file

---
+++ pattern_14.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print(int n){
+    for(int i=0;i<n;i++){
+        for(char ch='A'; ch<='A' + (n-i-1);ch++){
+            cout<<ch<<" ";
+    
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    print(n);
+}
