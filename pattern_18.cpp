5bbea9aeca8e52f689d0fdbdade8f94e307dd248
Stranger4uu <yashsaini4824@gmail.com>
2025-11-12 16:00:06 +0530

    added new pattern file

---
+++ pattern_18.cpp
@@ -0,0 +1,82 @@
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print(int n)
+{
+    int space = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < space; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        space += 2;
+        cout << "\n";
+
+    }
+
+    space = 8;
+    for (int i = 1; i <= n; i++)
+    {
+
+        // stars
+        for (int j = 1; j <=i; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < space; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+
+        for (int j = 1; j <=i; j++)
+        {
+            cout << "*";
+        }
+
+        space -= 2;
+        cout << "\n";
+
+    }
+}
+
+
+ 
+int main()
+{
+
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    print(n);
+}
