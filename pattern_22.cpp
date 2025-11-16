4434b9859e2060de8cb11023c011b60ca60fb3f6
Stranger4uu <yashsaini4824@gmail.com>
2025-11-16 23:08:23 +0530

    modified old file

--- pattern_22.cpp
+++ pattern_22.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+
+
+void print(int n){
     int rows = 5;
 
     for (int i = 0; i < rows; i++) {
         
-        // Print leading spaces
+        //spaces
         for (int space = 0; space < rows - i; space++) {
             cout << " ";
         }
 
         int value = 1;   // First value of each row
 
-        // Print values in each row
+        //value
         for (int j = 0; j <= i; j++) {
             cout << value << " ";
 
-            // Compute next value using formula: nCr
+        
             value = value * (i - j) / (j + 1);
         }
 
         cout << endl;
     }
 
-    return 0;
+}
+int main() {
+    int n;
+    print(n);
+   
 }
